58ad66260475052e5d2ffb285c3ee9541dfb2e49
cedric-winbush <cawinbushjr@gmail.com>
2020-06-17 16:18:21 -0500

    solves sliding_window

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -5,7 +5,35 @@ Returns: a List of integers
 def sliding_window_max(nums, k):
     # Your code here
 
-    pass
+    # k is the total number of elements in the sliding window
+    # window moves 1 space at a time to the right so need a counter
+    # to change start point of things
+    # counter must stop at end of array length minus k 
+    # place in temp list
+    # check the max value in each list
+    # in new list append max of the sliding amount
+
+    new_list = []
+    length = int(len(nums) - k)
+    beginning = 0
+    end = k
+    counter = 0
+    
+
+    while counter  <= length:
+        temp_list = nums[beginning:end]
+        counter += 1
+        beginning += 1
+        end += 1
+        print (temp_list)
+        max_value = max((temp_list))
+        new_list.append(max_value)
+
+    return new_list
+
+
+
+
 
 
 if __name__ == '__main__':
